732e9aa1f7799c7364d4f8aa3c3862f7cff61edd
Arie Sastra Hadiprawira <ariesastra@blade.co.id>
2020-11-09 19:34:31 +0700

    update funciton

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // base url to make request
 const instance = axios.create({
-    baseURL : "http://localhost:5001/clone-5c7be/us-central1/api",
+    // baseURL : "http://localhost:5001/clone-5c7be/us-central1/api",
+    baseURL : "https://us-central1-clone-5c7be.cloudfunctions.net/api",
     // mode: 'no-cors',
     headers: {
         'Access-Control-Allow-Origin': '*',
